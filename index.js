d1458d42030b795a790378e4e22979ad0dd98eee
konaga-k <priests2020@gmail.com>
2021-07-24 07:27:16 +0900

    Change method name of NearestStationFinder

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const NearestStationFinder = require('./lib/nearest-station-finder.js')
 
 const finder = new NearestStationFinder(process.argv[2])
 
-finder.findNearestStation().then(station => {
+finder.exec().then(station => {
   console.log(station)
 }).catch(error => {
   console.error(error)
